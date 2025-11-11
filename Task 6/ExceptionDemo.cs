b40a2855348b5c956f108e81cf125b4b408b8439
Prince <princeshrestha2111018@gmail.com>
2025-11-11 12:23:47 +0545

    Task 6

---
+++ Task 6/ExceptionDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class ExceptionDemo
+    {
+        public void ConvertToInteger()
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int number = Convert.ToInt32(input);
+                Console.WriteLine("You entered: " + number);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number format");
+            }
+            finally
+            {
+                Console.WriteLine("Program Executed");
+            }
+        }
+
+        public void CheckPassword()
+        {
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            try
+            {
+                if (password.Length < 6)
+                {
+                    throw new Exception("Password must be at least 6 characters.");
+                }
+                Console.WriteLine("Password satisfied the requirement");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Task 6/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new ExceptionDemo();
+
+            demo.ConvertToInteger();
+            Console.WriteLine();
+            demo.CheckPassword();
+        }
+    }
+}
