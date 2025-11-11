4be941b18b1e1b2c3d65e8894c96f9bcf10d6e7a
Prince <princeshrestha2111018@gmail.com>
2025-11-11 12:02:49 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DayChecker dayChecker = new DayChecker();
+        }
+    }
+}
