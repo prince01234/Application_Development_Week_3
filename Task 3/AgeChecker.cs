12fe29716d56637b5cde841c858f7236b8821939
Prince <princeshrestha2111018@gmail.com>
2025-11-11 11:49:19 +0545

    Task 3

---
+++ Task 3/AgeChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class AgeChecker
+    {
+        public AgeChecker()
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("You are a child");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("You are a teenager");
+            }
+            else
+            {
+                Console.WriteLine("You are an adult");
+            }
+        }
+    }
+}
