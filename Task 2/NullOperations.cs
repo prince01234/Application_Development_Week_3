6b26e475819f4ab9efa58c85e57ad721f2bf98ab
Prince <princeshrestha2111018@gmail.com>
2025-11-11 11:36:21 +0545

    Task 2

--- Task 2/NullOperations.cs
+++ Task 2/NullOperations.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace Task_2
 {
     internal class NullOperations
     {
+        public void PerformNullChecks()
+        {
+            String username = null;
+
+            // Using Ternary Operator
+            String userNameChecker = (username != null) ? username : "Username is not available";
+            Console.WriteLine(userNameChecker);
+
+            // Using Null-Coalescing Operator
+            String userNameChecker2 = username ?? "Username is not available";
+            Console.WriteLine(userNameChecker2);
+
+            // Using Null-Coalescing Assignment Operator
+            username ??= "DefaultUser";
+            Console.WriteLine(username);
+        }
     }
 }
+
+
---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            NullOperations nullOps = new NullOperations();
+            nullOps.PerformNullChecks();
+        }
+    }
+}
